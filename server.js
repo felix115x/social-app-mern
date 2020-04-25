59a488d89272deaac3f311596c77ebd3389b5e69
Felikss Kamisevs <felix115x@icloud.com>
2020-04-25 23:40:18 +0300

    Initial client commit WIP

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/database');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();    // database online
 
